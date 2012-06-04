283283301decb3db908169dbd14c035de3cb54e4
wangxiao <1988wangxiao@gmail.com>
2012-06-04 15:04:52 +0800

    addClass接口
    api+demo

--- src/baidu/dom/addClass.js
+++ src/baidu/dom/addClass.js
@@ -4,44 +4,31 @@
 
 ///import baidu;
 ///import baidu.dom;
-///import baidu.type;
 ///import baidu.each;
 
 baidu.dom.extend({
-    addClass: function(arg){
-    	//异常处理
-        if(arguments.length <= 0 || classNameString === null){return this;}
-        document.createDocumentFragment().appendChild(div);
-        for(var i = 0; i < args.length; i++){
-            type = baidu.type(args[i]);
-            switch(type){
-                case 'string':
-                    div.innerHTML = args[i];
-                    baidu.each(div.childNodes, function(item){
-                        fragment.appendChild(item);
-                    });
-                    break;
+    addClass: function(className){
+    	
+        //异常处理
+        if(arguments.length <= 0 || typeof className === 'function'){
+            return this;
+        };
 
-                case 'HTMLElement':
-                    fragment.appendChild(args[i]);
-                    break;
+        //对输入进行处理
+        className = String(className)
+                    .replace(/^\s+/g,'')
+                    .replace(/\s+$/g,'')
+                    .replace(/\s+/g,' ')
+                    .replace(/([.*+?^=!:${}()|[\]\/\\])/g, '\\$1');
 
-                case '$DOM':
-                case 'array':
-                    baidu.each(args[i], function(item){
-                        fragment.appendChild(item);
-                    });
-                    break;
-                    
-                case 'function':
-                    baidu.each(this, function(item, index){
-                        baidu.dom(item).append(args[i].call(item, index, item.innerHTML));
-                    });
-                    break;
-        }
+        var arr = className.split(' ');
 
         baidu.each(this, function(item){
-            item.appendChild(fragment.cloneNode(true));
+            for(var i = 0;i<arr.length;i++){
+                if(!new RegExp('\\b'+arr[i]+'\\b').test(item.className)){
+                    item.className += arr[i];
+                }
+            };
         });
 
         return this;
