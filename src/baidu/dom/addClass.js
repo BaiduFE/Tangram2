283a985db25a59f409bd7bd9b5650e8fc2b9674e
wangxiao <1988wangxiao@gmail.com>
2012-06-04 16:05:09 +0800

    addClass接口

--- src/baidu/dom/addClass.js
+++ src/baidu/dom/addClass.js
@@ -7,29 +7,40 @@
 ///import baidu.each;
 
 baidu.dom.extend({
-    addClass: function(className){
+    addClass: function(value){
     	
         //异常处理
-        if(arguments.length <= 0 || typeof className === 'function'){
+        if(arguments.length <= 0 ){
             return this;
         };
 
-        //对输入进行处理
-        className = String(className)
-                    .replace(/^\s+/g,'')
-                    .replace(/\s+$/g,'')
-                    .replace(/\s+/g,' ')
-                    .replace(/([.*+?^=!:${}()|[\]\/\\])/g, '\\$1');
+        switch(typeof value){
+            case 'string':
+                //对输入进行处理
+                value = String(value)
+                            .replace(/^\s+/g,'')
+                            .replace(/\s+$/g,'')
+                            .replace(/\s+/g,' ')
+                            .replace(/([.*+?^=!:${}()|[\]\/\\])/g, '\\$1');
 
-        var arr = className.split(' ');
+                var arr = value.split(' ');
 
-        baidu.each(this, function(item){
-            for(var i = 0;i<arr.length;i++){
-                if(!new RegExp('\\b'+arr[i]+'\\b').test(item.className)){
-                    item.className += arr[i];
-                }
-            };
-        });
+                baidu.each(this, function(item){
+                    for(var i = 0;i<arr.length;i++){
+                        if(!new RegExp('\\b'+arr[i]+'\\b').test(item.className)){
+                            item.className += (' '+arr[i]);
+                        }
+                    };
+                });
+            break;
+            case 'function':
+                baidu.each(this, function(item, index ,className){
+                    value.call(item, index, item.className);
+                });
+            break;
+            default:
+            break;
+        };
 
         return this;
     }
