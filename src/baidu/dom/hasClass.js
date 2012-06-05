9e9873181b6125f243fcd74d96842051d64bc9f9
wangxiao <1988wangxiao@gmail.com>
2012-06-05 10:16:04 +0800

    修改hasClass接口实现

--- src/baidu/dom/hasClass.js
+++ src/baidu/dom/hasClass.js
@@ -14,29 +14,13 @@ baidu.dom.extend({
             return this;
         };
 
-        //对输入进行处理
-        className = String(className)
-                    .replace(/^\s+/g,'')
-                    .replace(/\s+$/g,'')
-                    .replace(/\s+/g,' ')
-                    .replace(/([.*+?^=!:${}()|[\]\/\\])/g, '\\$1');
-
-        var arr = className.split(' ');
-        var result;
+        var result ;
 
         baidu.each(this, function(item){
-            for(var i = 0;i<arr.length;i++){
-                
-                if(!new RegExp('\\b'+arr[i]+'\\b').test(item.className)){
-                    //有一个不含有
-                    result = false;
-                    return;
-                };
-
-                //都含有
-                if(result){
-                    result = true;
-                };
+            if((' '+item.className+' ').indexOf(' '+className+' ') >-1 ){
+                result = true ;
+            }else{
+                result = false;
             }
         });
 
