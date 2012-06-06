9a499bceddba6726bdfb80bc4a612110852bfa38
wangxiao <1988wangxiao@gmail.com>
2012-06-06 09:27:47 +0800

    Revert "Revert "修改hasClass接口实现""
    This reverts commit 107b0a2da211bf28847725dd0bf824935dee6b28.

---
+++ src/baidu/dom/toggleClass.js
@@ -0,0 +1,47 @@
+/**
+ * @author wangxiao
+ */
+
+///import baidu;
+///import baidu.dom;
+///import baidu.each;
+
+baidu.dom.extend({
+    toggleClass: function(value){
+
+        //异常处理
+        if(arguments.length <= 0 ){
+            return this;
+        };
+
+        switch(typeof value){
+            case 'string':
+                //对输入进行处理
+                value = String(value)
+                            .replace(/^\s+/g,'')
+                            .replace(/\s+$/g,'')
+                            .replace(/\s+/g,' ')
+                            .replace(/([.*+?^=!:${}()|[\]\/\\])/g, '\\$1');
+
+                var arr = value.split(' ');
+
+                baidu.each(this, function(item){
+                    for(var i = 0;i<arr.length;i++){
+                        if(!new RegExp('\\b'+arr[i]+'\\b').test(item.className)){
+                            item.className += (' '+arr[i]);
+                        }
+                    };
+                });
+            break;
+            case 'function':
+                baidu.each(this, function(item, index ,className){
+                    baidu.dom(item).addClass(value.call(item, index, item.className));
+                });
+            break;
+            default:
+            break;
+        };
+
+        return this;
+    }
+});
