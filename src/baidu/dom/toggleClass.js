0e3da4fe4a6018c6fd5357f7927d0b7d0215d53e
wangxiao <1988wangxiao@gmail.com>
2012-06-06 14:01:13 +0800

    toggleClass接口

--- src/baidu/dom/toggleClass.js
+++ src/baidu/dom/toggleClass.js
@@ -7,36 +7,44 @@
 ///import baidu.each;
 
 baidu.dom.extend({
-    toggleClass: function(value){
-    	
+    toggleClass: function(value,status){
+    	var type = typeof value;
+        var status = (typeof status === 'undefined')? status : Boolean(status);
+
         if(arguments.length <= 0 ){
             baidu.each(this,function(item){
                 item.className = '';
             });
         };
 
-        switch(typeof arguments[1]){
+        switch(typeof value){
             case 'string':
                 var arr = value.split(' ');
-
                 baidu.each(this, function(item){
                     var str = item.className;
                     for(var i = 0;i<arr.length;i++){
 
                         //有这个className
-                        if((' '+str+' ').indexOf(' '+arr[i]+' ') > -1){
+                        if(((' '+str+' ').indexOf(' '+arr[i]+' ') > -1)&&(typeof status === 'undefined')){
                             item.className = str.replace(arr[i],'');
-                        }else{
+
+                        }else if(((' '+str+' ').indexOf(' '+arr[i]+' ') === -1)&&(typeof status === 'undefined')){
                             item.className += arr[i];
+
+                        }else if(((' '+str+' ').indexOf(' '+arr[i]+' ') === -1)&&(status === true)){
+                            item.className += arr[i];
+
+                        }else if(((' '+str+' ').indexOf(' '+arr[i]+' ') > -1)&&(status === false)){
+                            item.className = str.replace(arr[i],'');
+
                         }
                     };
                 });
-
             break;
             case 'function':
 
-                baidu.each(this, function(item, index ,className){
-                    baidu.dom(item).toggleClass(value.call(item, index, item.className));
+                baidu.each(this, function(item, index){
+                    baidu.dom(item).toggleClass(value.call(item, index, item.className),status);
                 });
 
             break;
