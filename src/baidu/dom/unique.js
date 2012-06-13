fd0f899e04d5627198328f836f84200a287e1932
huangfangrong <huangfangrong@baidu.com>
2012-06-13 14:57:46 +0800

    [Featrue] 添加新接口： baidu.dom.add(); baidu.dom.children(); baidu.dom.closest(); baidu.dom.first(); baidu.dom.has(); baidu.dom.last(); baidu.dom.next(); baidu.dom.not(); baidu.dom.slice(); baidu.dom.unique();

---
+++ src/baidu/dom/unique.js
@@ -0,0 +1,21 @@
+/// import baidu.dom;
+/// import baidu.unique;
+
+/**
+ * @fileoverview
+ * @name baidu.dom.unique
+ * @author meizz
+ * @create 2012-05-28
+ * @modify
+ */
+
+/**
+ * 去重
+ * @param   {Function}      fn(a, b)    [可选]
+ * @return  {TangramDom}    new TangramDom
+ */
+baidu.dom.extend({
+    unique : function (fn) {
+        return baidu.dom(baidu.unique(this.get(), fn));
+    }
+});
