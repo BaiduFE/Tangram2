b835a6fda1803b252ced304e4945727b495a424c
linlingyu <linlingyu@baidu.com>
2012-06-13 20:44:36 +0800

    [Feature] add offset

--- src/baidu/support.js
+++ src/baidu/support.js
@@ -2,7 +2,7 @@
  * @author linlingyu
  */
 ///import baidu;
-///import baidu.dom.
+///import baidu.dom;
 
 baidu.support = baidu.support || (function(){
     var div = document.createElement('div'),
@@ -35,9 +35,9 @@ baidu.support = baidu.support || (function(){
         div.innerHTML = '<div style="'+ styleString +'display:bloack;"><div style="'+ paddingMarginBorder +'0;display:block;overflow:hidden;"></div></div><table style="'+ styleString +'" cellpadding="0" cellspacing="0"><tr><td></td></tr></table>';
         outer = div.firstChild;
         inner = outer.firstChild;
-        table = div.getElementsByTagName('table')[0];
-        support.hasBorder = inner.offsetTop >= 5;
-        support.hasTableCellBorder = table.rows[0].cells[0].offsetTop >= 5;
+        table = outer.nextSibling;
+        support.hasBorderWidth = inner.offsetTop >= 5;//opera
+        support.hasTableCellBorderWidth = table.rows[0].cells[0].offsetTop >= 5;//ie,firefox
         inner.style.position = 'fixed';
         inner.style.top = '20px';
         support.fixedPosition = inner.offsetTop === 20 || inner.offsetTop === 15;
