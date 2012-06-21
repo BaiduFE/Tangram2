ab6ddffd3dada93d4e53d1dd36f6d64abe7c4d95
wangxiao <1988wangxiao@gmail.com>
2012-06-21 10:54:36 +0800

    迁移ajax,string,fn

--- src/baidu/fn/bind.js
+++ src/baidu/fn/bind.js
@@ -4,7 +4,6 @@
  */
 
 ///import baidu.fn;
-
 ///import baidu.lang.isString;
 
 /** 
--- src/baidu/string/trim.js
+++ src/baidu/string/trim.js
@@ -1,11 +1,6 @@
-/*
- * Tangram
- * Copyright 2009 Baidu Inc. All rights reserved.
- * 
- * path: baidu/string/trim.js
- * author: dron, erik
- * version: 1.1.0
- * date: 2009/11/15
+/**
+ * @author wangxiao
+ * @email  1988wangxiao@gmail.com
  */
 
 ///import baidu.string;
@@ -26,11 +21,11 @@
 
 (function () {
     var trimer = new RegExp("(^[\\s\\t\\xa0\\u3000]+)|([\\u3000\\xa0\\s\\t]+\x24)", "g");
-    
-    baidu.string.trim = function (source) {
-        return String(source)
-                .replace(trimer, "");
-    };
+    baidu.string.extend({
+    trim : function () {
+        return this.replace(trimer, "");
+    }
+    });
 })();
 
 // 声明快捷方法
