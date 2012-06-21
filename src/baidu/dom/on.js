6faef4dfefe4de4472fdd241511f65c062d54492
ChineseDron <dron@qq.com>
2012-06-21 11:32:13 +0800

    [Change] some files

--- src/baidu/dom/on.js
+++ src/baidu/dom/on.js
@@ -9,45 +9,43 @@
 ///import baidu.dom._eventBase;
 
 baidu.dom.extend({
-	on: function(){
-		var eb = baidu.dom._eventBase;
-	    return function(events, selector, data, fn){
+	on: function(events, selector, data, fn){
+    	var eb = baidu.dom._eventBase;
 
-			if(typeof events == "string"){
+		if(typeof events == "string"){
 
-			    if(typeof selector == "function"){
-			        fn = selector;
-			        selector = null;
-			    }else if(typeof data == "function"){
-			    	fn = data;
-			    	data = null;
-			    }
+		    if(typeof selector == "function"){
+		        fn = selector;
+		        selector = null;
+		    }else if(typeof data == "function"){
+		    	fn = data;
+		    	data = null;
+		    }
 
-			    if(typeof selector == "object"){
-			        data = selector;
-			        selector = null;
-			    }
+		    if(typeof selector == "object"){
+		        data = selector;
+		        selector = null;
+		    }
 
-			    events = events.split(/[ ,]/);
+		    events = events.split(/[ ,]/);
 
-			    this.each(function(){
-			    	var me = this;
-			        baidu.each(events, function(event){
-			            eb.add(me, event, fn, selector, data);
-			        });
-			    });
+		    this.each(function(){
+		    	var me = this;
+		        baidu.each(events, function(event){
+		            eb.add(me, event, fn, selector, data);
+		        });
+		    });
 
-			}else if(typeof events == "object"){
+		}else if(typeof events == "object"){
 
-				var me = this;
-				
-				baidu.each(events, function(fn, events){
-				    me.on(events, selector, data, fn);
-				});
+			var me = this;
+			
+			baidu.each(events, function(fn, events){
+			    me.on(events, selector, data, fn);
+			});
 
-			}
-
-			return this;
 		}
-	}()
+
+		return this;
+	}
 });
