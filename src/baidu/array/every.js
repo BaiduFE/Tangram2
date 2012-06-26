703a339d509e114b0bba4ee4fdf1aca68381e3b2
huangfangrong <huangfangrong@baidu.com>
2012-06-26 18:24:25 +0800

    [Feature] 添加 array.hash 和 array.reduce 两个新接口

--- src/baidu/array/every.js
+++ src/baidu/array/every.js
@@ -1,16 +1,17 @@
 ///import baidu.array;
 
+Array.prototype.every = function(iterator, context) {
+    var i, n;
 
-baidu.array.extend({
-    every : function (iterator, context) {
-        var i = 0,
-            len = this.length;
-
-        for (; i < len; i++) {
-            if (i in this && !iterator.call(context || this, this[i], i, this)) {
-                return false;
-            }
+    for (i=0, n=this.length; i<n; i++) {
+        if (!iterator.call(context || this, this[i], i, this)) {
+            return false;
         }
-        return true;
     }
-});
+    return true;
+};
+
+// TODO: delete in tangram 3.0
+baidu.array.every = function(array, iterator, context) {
+    return array.every(iterator, context);
+};
