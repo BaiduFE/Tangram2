f40ab5f2b0db797e3620401a2ad4d343cb68ded9
huangfangrong <huangfangrong@baidu.com>
2012-07-05 17:32:55 +0800

    [Feature] baidu.dom.createElements 将HTMLString转换成HTMLElement对象 [Change] baidu.dom.js(HTMLString) 此种分支的[TODO]关闭

--- src/baidu/dom.js
+++ src/baidu/dom.js
@@ -96,7 +96,9 @@ function(selector, context) {
 
         // HTMLString
         if (selector.charAt(0) == "<" && selector.charAt(selector.length - 1) == ">" && selector.length > 3) {
-            // [TODO] 0531 HTMLString 模式暂缓
+            if ( baidu.dom.createElements ) {
+                baidu.merge( me, baidu.dom.createElements(selector) );
+            }
 
         // baidu.query
         } else {
