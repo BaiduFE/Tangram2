ab8cb906daf07448f2c5a552bc3a2a92662511dc
huangfangrong <huangfangrong@baidu.com>
2012-07-11 09:21:29 +0800

    [Change] baidu.paramCheck.js 升级

--- src/baidu/paramCheck.js
+++ src/baidu/paramCheck.js
@@ -14,6 +14,29 @@
  * @param   {String}    regString     被分析的形参匹配字符串
  * @param   {String}    moduleName    被分析的模块名
  */
-baidu.paramCheck = function(regs, namespace) {
+baidu.paramCheck = function() {
+    var global = window[baidu.guid],
+        maps = global._mapsReg = global._mapsReg || {};
 
-};
+    return function(regString, namespace){
+        var caller, arg, i, n,
+            types = "",
+            reg = maps[ regString ] || ( maps[ regString ] = new RegExp(regString, "i") );
+
+        if ( caller = arguments.callee.caller ) {
+            arg = caller.arguments;
+
+            for ( i=0, n=arg.length; i<n; i++ ) {
+                types += "," + baidu.type( arg[ i ] );
+            }
+
+            types.length && ( types = types.substr( 1 ) );
+
+            if ( reg.test( types ) ) {
+                return;
+            }
+
+            throw new Error( "Parameter type mismatch.\nPosition : " + ( namespace || caller.toString() ) );
+        }
+    };
+}();
