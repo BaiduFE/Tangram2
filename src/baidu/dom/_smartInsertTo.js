16ce293c9e3847c6c2ceabb55bbe98bca40a1cfe
linlingyu <linlingyu@baidu.com>
2012-07-13 23:27:55 +0800

    [Change] 修改_buildElements为createElements并删除_buildElements

--- src/baidu/dom/_smartInsertTo.js
+++ src/baidu/dom/_smartInsertTo.js
@@ -5,32 +5,25 @@
 ///import baidu.type;
 ///import baidu.dom.clone;
 ///import baidu.dom.getDocument;
-///import baidu.dom._buildElements;
+///import baidu.dom.createElements;
 ///import baidu.dom._smartInsert;
 ///import baidu.dom.contains;
 
-baidu.dom._smartInsertTo = baidu.dom._smartInsertTo || function(tang, target, callback, orie){
-    baidu.type(target) === 'string'
-        && target.charAt(0) === '<'
-        && target.charAt(target.length - 1) === '>'
-        && target.length > 2
-        && (target = baidu.dom._buildElements([target], tang.getDocument() || document));
+baidu.dom._smartInsertTo = function(tang, target, callback, orie){
     var insert = baidu.dom(target),
         first = insert[0],
-        len, tangDom;
+        tangDom;
         
-    if(orie && first && (!first.parentNode || (first.nodeType !== 3
-        && !baidu.dom.contains(document.documentElement, first)))){
-            orie = orie === 'before';
-            tangDom = baidu.merge(orie ? tang : insert, !orie ? tang : insert);
-            if(tang !== tangDom){
-                tang.length = 0;
-                baidu.merge(tang, tangDom);
-            }
+    if(orie && first && (!first.parentNode || first.parentNode.nodeType === 11)){
+        orie = orie === 'before';
+        tangDom = baidu.merge(orie ? tang : insert, !orie ? tang : insert);
+        if(tang !== tangDom){
+            tang.length = 0;
+            baidu.merge(tang, tangDom);
+        }
     }else{
-        len = insert.length;
-        for(var i = 0; i < len; i++){
-            baidu.dom._smartInsert(baidu.dom(insert[i]), i > 0 ? tang.clone(true) : tang, callback);
+        for(var i = 0, item; item = insert[i]; i++){
+            baidu.dom._smartInsert(baidu.dom(item), i > 0 ? tang.clone(true) : tang, callback);
         }
     }
 };
