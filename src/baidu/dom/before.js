1fe16f8edc6f9e20e8f5e91185409cbc06938f1c
linlingyu <linlingyu@baidu.com>
2012-07-19 19:06:08 +0800

    [Change] 增加第一批接口的注释和参数检查

--- src/baidu/dom/before.js
+++ src/baidu/dom/before.js
@@ -5,9 +5,26 @@
 ///import baidu.dom._buildElements;
 ///import baidu.dom.getDocument;
 ///import baidu.merge;
-
+/**
+ * @description 在匹配的每个DOM元素前面插入新的内容
+ * @function 
+ * @name baidu.dom().before()
+ * @grammar baidu.dom(args).before(content[, content])
+ * @param {HTMLString|Element|TangramDom} content 支持一个DOM元素或是一段HTMLString或是一个TangramDom对象
+ * @param {HTMLString|Array|Element|TangramDom} content 支持一个或多个DOM元素或是DOM元素的数组或是一段HTMLString或是一个TangramDom对象
+ * @return {TangramDom} 返回之前匹配元素的TangramDom对象
+ */
+/**
+ * @description 在匹配的每个DOM元素前面插入新的内容
+ * @function 
+ * @name baidu.dom().before()
+ * @grammar baidu.dom(args).before(function(index,html))
+ * @param {function} fn 支持一个函数作为参数，函数最终需要返回一个HTMLString|Element|TangramDom
+ * @return {TangramDom} 返回之前匹配元素的TangramDom对象
+ */
 baidu.dom.extend({
     before: function(){
+        baidu.paramCheck('^(?:string|function|HTMLElement|\\$DOM)(?:,(?:string|array|HTMLElement|\\$DOM))*$', 'baidu.dom.before');
         var parentNode = this[0] && this[0].parentNode,
             array = !parentNode && [], set;
         
--- src/baidu/dom/remove.js
+++ src/baidu/dom/remove.js
@@ -4,6 +4,22 @@
 ///import baidu.dom.filter;
 ///import baidu.id;
 ///import baidu.dom._eventBase;
+/**
+ * @description 将匹配到的DOM元素从文档中移除，并移除对应的DOM元素的事件
+ * @function 
+ * @name baidu.dom().remove()
+ * @grammar baidu.dom(args).remove([selector])
+ * @param {String} selector 一个字符串的选择器，对前面匹配到的DOM元素再做进一步的过滤
+ * @return {TangramDom} 接口最终返回之前匹配元素的TangramDom对象
+ */
+/**
+ * @description 从DOM树上移除目标元素
+ * @function 
+ * @name baidu.dom.remove
+ * @grammar baidu.dom.remove(element)
+ * @param {String|Element} element 需要移除的元素或元素的id
+ * @return {Element} 被移除的DOM元素
+ */
 
 baidu.dom.extend({
     remove: function(){
@@ -18,6 +34,8 @@ baidu.dom.extend({
         }
         //
         return function(selector, keepData){
+            arguments.length > 0
+                && baidu.paramCheck('^string(?:,boolean)?$', 'baidu.dom.remove');
             var array = selector ? this.filter(selector) : this,
                 elements;
             for(var i = 0, ele; ele = array[i]; i++){
