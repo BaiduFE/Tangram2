aa338c71589bc5371bfebf85b778e2a3e6a2f1a5
wangxiao <1988wangxiao@gmail.com>
2012-07-24 11:41:20 +0800

    html增加去掉事件方法

--- src/baidu/dom/html.js
+++ src/baidu/dom/html.js
@@ -73,7 +73,7 @@ baidu.dom.extend({
             if(result){
                 return;
             };
-
+            var tangramDom = bd(elem);
             switch(typeof value){
                 case 'undefined':
         
@@ -101,7 +101,7 @@ baidu.dom.extend({
                             // Remove element nodes and prevent memory leaks
                             if ( elem.nodeType === 1 ) {
                                 //jQuery.cleanData( elem.getElementsByTagName( "*" ) );
-                                //Todo 清除事件
+                                tangramDom.empty();
                                 elem.innerHTML = value;
                             }
 
@@ -121,7 +121,6 @@ baidu.dom.extend({
 
                     //set all
                     isSet = true;
-                    var tangramDom = bd(elem);
                     tangramDom.html(value.call(elem, index, tangramDom.html()));
                 break;
 
