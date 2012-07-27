1046db9bae386cace35fe7f60156a7f37ad689a4
wangxiao <1988wangxiao@gmail.com>
2012-07-27 15:06:46 +0800

    再次merge

--- src/baidu/ajax/get.js
+++ src/baidu/ajax/get.js
@@ -8,9 +8,8 @@
 /**
  * @description 发送一个get请求
  * @function 
- * @name baidu.ajax.get
- * @grammar baidu.ajax(url).get([function(xhr, responseText)])
- * @param {String} url 发送请求的url地址
+ * @name baidu.ajax().get()
+ * @grammar baidu.ajax(url).get(function(xhr, responseText))
  * @param {function} onsuccess 请求成功之后的回调函数，函数接收两个参数xhr是一个XMLHttpRequest对象，responseText是请求的返回内容
  * @return {XMLHttpRequest} XMLHttpRequest对象
  */
