b9d8f377bc0d135ace13c591a1e398da8a3552c8
wangxiao <1988wangxiao@gmail.com>
2012-08-07 17:09:42 +0800

    修改注释

--- src/baidu/dom/one.js
+++ src/baidu/dom/one.js
@@ -13,7 +13,7 @@
  * @param {String} type 事件名称，如果是多个事件，以空格或半角逗号隔开
  * @param {Object} data 事件触发时携带的数据，JSON 格式，此参数可选。
  * @param {Function} fn 事件触发函数，fn 接受一个参数 e，为 baidu.event() 事件对象实例
- * @return TangramDom 
+ * @return {TangramDom} 返回之前匹配元素的TangramDom对象 
  */
 
 baidu.dom.extend({
