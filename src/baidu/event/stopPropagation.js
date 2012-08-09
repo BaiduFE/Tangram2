4c733371966e280589db9fed02185d59cd1e1b68
linlingyu <linlingyu@baidu.com>
2012-08-09 18:14:56 +0800

    [Feature] 增加event的若干老接口

---
+++ src/baidu/event/stopPropagation.js
@@ -0,0 +1,28 @@
+/*
+ * Tangram
+ * Copyright 2009 Baidu Inc. All rights reserved.
+ * 
+ * path: baidu/event/stopPropagation.js
+ * author: erik
+ * version: 1.1.0
+ * date: 2009/11/23
+ */
+
+///import baidu.event;
+
+/**
+ * 阻止事件传播
+ * @name baidu.event.stopPropagation
+ * @function
+ * @grammar baidu.event.stopPropagation(event)
+ * @param {Event} event 事件对象
+ * @see baidu.event.stop,baidu.event.preventDefault
+ */
+baidu.event.stopPropagation = function (event) {
+    event.originalEvent && (event = event.originalEvent);
+    if (event.stopPropagation) {
+        event.stopPropagation();
+    } else {
+        event.cancelBubble = true;
+    }
+};
