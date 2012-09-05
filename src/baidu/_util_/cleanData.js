c7858d1e22eba9d2c01a15435d5e49680373661b
linlingyu <linlingyu@baidu.com>
2012-09-05 11:44:11 +0800

    [Change] eventBase暂时未定

--- src/baidu/_util_/cleanData.js
+++ src/baidu/_util_/cleanData.js
@@ -7,7 +7,7 @@ baidu._util_.cleanData = function(array){
     for(var i = 0, ele; ele = array[i]; i++){
         tangId = baidu.id(ele, 'get');
         if(!tangId){continue;}
-        baidu._util_.eventBase.removeAll(ele);
+        baidu.dom._eventBase.removeAll(ele);
         baidu.id(ele, 'remove');
     }
 }
