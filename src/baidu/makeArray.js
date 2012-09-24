5e2000dd1e2de6831f388664d222599d5e5f8d2d
linlingyu <linlingyu@baidu.com>
2012-09-24 19:10:43 +0800

    [bugfix] 注释修改

--- src/baidu/makeArray.js
+++ src/baidu/makeArray.js
@@ -4,7 +4,14 @@
 
 ///import baidu.merge;
 ///import baidu.type;
-
+/**
+ * @description 转换一个类似数组的对象成为真正的JavaScript数组
+ * @function 
+ * @name baidu.makeArray
+ * @grammar baidu.makeArray(obj)
+ * @param {Object} obj 转换成一个原生数组的任何对象
+ * @return {Array} 一个转换后的数组
+ */
 baidu.makeArray = function(array, results){
     var ret = results || [];
     if(!array){return ret;}
