dc70a8beac79cc875ca04015645edad7de16fe0f
linlingyu <linlingyu@baidu.com>
2012-09-24 13:21:46 +0800

    [Feature] 增加ajax的get和post

---
+++ src/baidu/_util_/smartAjax.js
@@ -0,0 +1,18 @@
+///import baidu.ajax;
+///import baidu.type;
+baidu._util_.smartAjax = baidu._util_.smartAjax || function(method){
+    return function(url, data, callback, type){
+        if(baidu.type(data) === 'function'){
+            type = type || callback;
+            callback = data;
+            data = undefined;
+        }
+        baidu.ajax({
+            type: method,
+            url: url,
+            data: data,
+            success: callback,
+            dataType: type
+        });
+    };
+}
