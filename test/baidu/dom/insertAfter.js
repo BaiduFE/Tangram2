cfcafd407f164a68ff47d77d0ddb45d8afd935bd
linlingyu <linlingyu@baidu.com>
2012-10-13 15:21:14 +0800

    [BugFix] 修改width和height接口不支持传入小数的问题 [BugFix] 修改ajax的demo使用了错误的参数名称

--- test/baidu/dom/insertAfter.js
+++ test/baidu/dom/insertAfter.js
@@ -76,7 +76,7 @@ test("老接口：id",function(){
 	baidu.dom.insertAfter(a,'div_id');
 	equal(div.nextSibling,a,"insert a after div");
 	equal(a.previousSibling,div,"insert a after div-2");
-	baidu.dom.insertAfter(p,'#img_id');
+	baidu.dom.insertAfter(p,'img_id');
 	equal(img.nextSibling,p,"insert p after img");
 	equal(p.previousSibling,img,"insert p after img-2");
 	document.body.removeChild(div);
