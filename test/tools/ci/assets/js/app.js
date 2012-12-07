05cba95b132833ca33c0da5f33148e8c67428f62
jianling <zhaochengyang@baidu.com>
2012-12-07 15:15:07 +0800

    ﻿[Bugfix]修复测试工具的bug

--- test/tools/ci/assets/js/app.js
+++ test/tools/ci/assets/js/app.js
@@ -110,9 +110,12 @@
          * 单元测试
          */
         unitTest: function(node){
-            var api = node.data.dir.replace('../../../src/', '').replace('.js', '').replace('/', '.'),
+            var api = node.data.dir.replace('../../../src/', '').replace('.js', '').replace(/\//g, '.'),
                 unitUrl = location.href.replace('index.php', '') + '../br/run.php?case=' + api;
-            
+                
+            if($("#J_unitTestFrame").length === 0){
+                $('J_unitTest').html('<iframe id="J_unitTestFrame" src="" frameborder="no" scrolling="auto" ></iframe>');
+            }
             $("#J_unitTestFrame").attr('src', unitUrl);
             var interval = setInterval(function(){
                 try{
