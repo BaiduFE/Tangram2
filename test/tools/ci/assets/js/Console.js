c364cfe5912d9f184806e534135989ba81c8de47
jianling <zhaochengyang@baidu.com>
2012-12-11 00:12:05 +0800

    ﻿[CHange]修改测试工具的控制台

--- test/tools/ci/assets/js/Console.js
+++ test/tools/ci/assets/js/Console.js
@@ -1,6 +1,8 @@
 define(function(require, exports) {
 
     var consoleOpened = false;
+    var filter = 'All';
+    var logTpl = '<p class="console-item {{log.level}}"><span class="level">[{{log.level}}]</span><span class="time">{{log.time}}</span><span class="desc">{{log.desc}}</span><span class="api">{{log.api}}</span></p>';
 
     /**
      * 初始化控制台
@@ -15,6 +17,39 @@ define(function(require, exports) {
                 $('#J_consoleWrap').removeClass('console-close').addClass('console-open');
                 consoleOpened = true;
             }
+        });
+
+        $('#J_clearConsole').click(function(){
+            clear();
+        });
+
+        $('.console-filter').click(function(){
+            filter = $(this).attr('filter');
+
+            $('.console-item').hide();
+            switch(filter){
+                case 'All':
+                    $('.console-item').show();
+                    break;
+                case 'Pass':
+                    $('.console .pass').show();
+                    break;
+                case 'Error':
+                    $('.console .error').show();
+                    break;
+                case 'Warning':
+                    $('.console .warning').show();
+                    break;
+                default:
+                    break;
+            }
+            
+            $('.console-filterWrap .focus').removeClass('focus');
+            $(this).addClass('focus');
+        }).mouseover(function(){
+            $(this).addClass('hover');
+        }).mouseout(function(){
+            $(this).removeClass('hover');
         });
 
     };
@@ -43,8 +78,10 @@ define(function(require, exports) {
     /**
      * 显示Log
      */
-    var log = function() {
+    var log = function(data) {
+        var _html = Mustache.render(logTpl, data);
 
+        $('#J_console').append(_html);
     };
 
     /**
@@ -55,20 +92,16 @@ define(function(require, exports) {
             count = 0,
             _list = {};
 
-        if(failureList.length){
-            $(failureList).each(function(index, item){
-                var api = item.data.dir.replace('../../../src/', '');
-                if(_list[api]){return;};
-                _list[api] = true;
-                count++;
-                html += '<li>' + api + '</li>';
-            });
-            html += '</ul>';
+        $(failureList).each(function(index, item){
+            var api = item.data.dir.replace('../../../src/', '');
+            if(_list[api]){return;};
+            _list[api] = true;
+            count++;
+            html += '<li>' + api + '</li>';
+        });
+        html += '</ul>';
 
-            html = '<p>共有' + count + '个接口检查不通过：<p><ul>' + html;
-        }else{
-            html = '<p>所有文件全部检查通过。</p>';
-        }
+        html = '<p>共有' + count + '个接口检查不通过：<p><ul>' + html;
         
         $('#J_' + currentCheckMode).html(html);
     };
