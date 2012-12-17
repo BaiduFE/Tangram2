7a298bba82a9c3615c1f8e213c43002ba31bda54
linlingyu <linlingyu@baidu.com>
2012-12-17 19:23:24 +0800

    [Change] baidu.id去掉不再使用的分支代码 [TestCase] 增加测试用例提高覆盖率

--- test/baidu/base/Class.js
+++ test/baidu/base/Class.js
@@ -15,4 +15,43 @@ test("基类 调用与声明", function(){
             this.showed = true;
         }
     };
+    ok(true, '过程正确');
+});
+
+test('事件注册，派发和删除', function(){
+    expect(2);
+    var instance = new baidu.base.Class();
+    instance.on('defaultEvent', function(evt){
+        ok(evt.target === instance, 'fire event');
+    });
+    instance.fire('defaultEvent');
+    instance.off('defaultEvent');
+    instance.fire('defaultEvent');
+    ok(baiduInstance(instance.guid) === instance, 'It is same instance');
+    instance.dispose();
+});
+
+test('调用一次的事件', function(){
+    expect(2);
+    var c = new baidu.base.Class();
+    c.one('oneevent', function(){
+        ok(true, 'fire default event');
+    });
+    c.fire('oneevent');
+    c.fire('oneevent');
+    c.once('onceevent', function(){
+        ok(true, 'fire default event');
+    });
+    c.fire('onceevent');
+    c.fire('onceevent');
+});
+
+test('析构', function(){
+    var instance = new baidu.base.Class();
+    instance.on('defaultEvent', function(){
+        ok(false, 'fire event');
+    });
+    instance.dispose();
+    instance.fire('defaultEvent');
+    ok(true, 'instance dispose');
 });
