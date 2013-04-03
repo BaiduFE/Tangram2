3c5b41f4f724e93ae4e381afee34401ed1770a80
linlingyu <linlingyu@baidu.com>
2013-04-03 15:16:38 +0800

    [Change] 开放cssHook，cssNumber，增加setPositiveNumber

---
+++ src/baidu/_util_/cssNumber.js
@@ -0,0 +1,12 @@
+///import baidu._util_;
+baidu._util_.cssNumber = {
+    'columnCount': true,
+    'fillOpacity': true,
+    'fontWeight': true,
+    'lineHeight': true,
+    'opacity': true,
+    'orphans': true,
+    'widows': true,
+    'zIndex': true,
+    'zoom': true
+};
--- src/baidu/dom/styleFixer.js
+++ src/baidu/dom/styleFixer.js
@@ -1,96 +1,26 @@
 /**
  * @author linlingyu
  */
-
-///import baidu.dom.getCurrentStyle;
-///import baidu.extend;
-///import baidu.forEach;
-///import baidu._util_.support;
-///import baidu._util_.getWidthOrHeight;
+///import baidu._util_.cssHooks;
+///import baidu._util_.cssNumber;
 ///import baidu.type;
 ///import baidu.string.toCamelCase;
+///import baidu._util_.support;
 
 baidu.dom.styleFixer = function(){
-    var alpha = /alpha\s*\(\s*opacity\s*=\s*(\d{1,3})/i,
-        nonpx = /^-?(?:\d*\.)?\d+(?!px)[^\d\s]+$/i,
-        cssNumber = 'fillOpacity,fontWeight,opacity,orphans,widows,zIndex,zoom',
-        anchor = baidu._util_.support.dom.a,
+    var style = baidu._util_.style,
+        cssHooks = baidu._util_.cssHooks,
+        cssNumber = baidu._util_.cssNumber,
         cssProps = {
-            'float': !!anchor.style.cssFloat ? 'cssFloat' : 'styleFloat'
-        },
-        cssMapping = {
-            fontWeight: {normal: 400, bold: 700, bolder: 700, lighter: 100}
-        },
-        cssHooks = {
-            opacity: {},
-            width: {},
-            height: {},
-            fontWeight: {
-                get: function(ele, key){
-                    var ret = style.get(ele, key);
-                    return cssMapping.fontWeight[ret] || ret;
-                }
-            }
-        },
-        style = {
-            set: function(ele, key, val){ele.style[key] = val;}
+            'float': !!baidu._util_.support.dom.a.style.cssFloat ? 'cssFloat' : 'styleFloat'
         };
-    baidu.extend(cssHooks.opacity, /^0.5/.test(anchor.style.opacity) ? {
-        get: function(ele, key){
-            var ret = baidu.dom(ele).getCurrentStyle(key);
-            return ret === '' ? '1' : ret;
-        }
-    } : {
-        get: function(ele){
-            return alpha.test((ele.currentStyle || ele.style).filter || '') ? parseFloat(RegExp.$1) / 100 : '1';
-        },
-        set: function(ele, key, value){
-            var filterString = (ele.currentStyle || ele.style).filter || '',
-                opacityValue = value * 100;
-                ele.style.zoom = 1;
-                ele.style.filter = alpha.test(filterString) ? filterString.replace(alpha, 'Alpha(opacity=' + opacityValue)
-                    : filterString + ' progid:dximagetransform.microsoft.Alpha(opacity='+ opacityValue +')';
-        }
-    });
-    //
-    baidu.forEach(['width', 'height'], function(item){
-        cssHooks[item] = {
-            get: function(ele){
-                return baidu._util_.getWidthOrHeight(ele, item) + 'px';
-            },
-            set: function(ele, key, val){
-                baidu.type(val) === 'number' && val < 0 && (val = 0);
-                style.set(ele, key, val);
-            }
-        };
-    });
-    
-    baidu.extend(style, document.documentElement.currentStyle? {
-        get: function(ele, key){
-            var val = baidu.dom(ele).getCurrentStyle(key),
-                defaultLeft;
-            if(nonpx.test(val)){
-                defaultLeft = ele.style.left;
-                ele.style.left = key === 'fontSize' ? '1em' : val;
-                val = ele.style.pixelLeft + 'px';
-                ele.style.left = defaultLeft;
-            }
-            return val;
-        }
-    } : {
-        get: function(ele, key){
-            return baidu.dom(ele).getCurrentStyle(key);
-        }
-    });
-    
-    //
     return function(ele, key, val){
-        var origKey = baidu.string(key).toCamelCase(),
+        var origKey = baidu.string.toCamelCase(key),
             method = val === undefined ? 'get' : 'set',
             origVal, hooks;
         origKey = cssProps[origKey] || origKey;
-        origVal = baidu.type(val) === 'number' && !~cssNumber.indexOf(origKey) ? val + 'px' : val;
+        origVal = baidu.type(val) === 'number' && !cssNumber[origKey] ? val + 'px' : val;
         hooks = cssHooks.hasOwnProperty(origKey) && cssHooks[origKey][method] || style[method];
         return hooks(ele, origKey, origVal);
-    };
+    }
 }();
