c475dec6f97e7ebea39a08ec50a34194a1302272
linlingyu <linlingyu@baidu.com>
2013-04-24 13:43:54 +0800

    [Chang] 更改each为forEach

--- src/plugin/fx/animation.js
+++ src/plugin/fx/animation.js
@@ -6,7 +6,7 @@
 ///import baidu.dom.css;
 ///import baidu.dom.contains;
 ///import baidu.dom.data;
-///import baidu.each;
+///import baidu.forEach;
 ///import baidu.extend;
 ///import baidu.deferred;
 ///import baidu.isFunction;
@@ -63,7 +63,7 @@
         };
 
     function createTweens( animation, props ) {
-        baidu.each( props, function( prop, value ) {
+        baidu.forEach( props, function( value, prop ) {
             var collection = ( tweeners[ prop ] || [] ).concat( tweeners[ "*" ] ),
                 index = 0,
                 length = collection.length;
