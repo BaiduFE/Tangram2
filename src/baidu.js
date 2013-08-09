7a7277199baccca1a25bc20a2614b10fb04f49ff
linlingyu <linlingyu@baidu.dom>
2013-08-09 13:06:15 +0800

    [Change] 修改版本号为2.0.2.5

--- src/baidu.js
+++ src/baidu.js
@@ -111,7 +111,7 @@
 
 var T, baidu = T = baidu || function(q, c) { return baidu.dom ? baidu.dom(q, c) : null; };
 
-baidu.version = '2.0.2.4';
+baidu.version = '2.0.2.5';
 baidu.guid = "$BAIDU$";
 baidu.key = "tangram_guid";
 
